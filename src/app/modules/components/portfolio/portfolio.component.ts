92239c393b1e8408c1a53a29883bc7c45ddb1f3a
ivanretamozo <retamozoi445@gmail.com>
2022-08-26 18:15:50 -0300

    ivn-movies

--- src/app/modules/components/portfolio/portfolio.component.ts
+++ src/app/modules/components/portfolio/portfolio.component.ts
@@ -9,15 +9,22 @@ import { cardProyects } from '../interfaces/interfaces';
 export class PortfolioComponent {
     cards: cardProyects[] = [
         {
+            title: 'ivn-movies',
+            desc: 'ivn-movies es una SPA  que permite ver la actualidad del cine y series, y mas,corre gracias a la api de Mapbox',
+            img: '../../../../assets/MapWorld.png',
+            demoLink: 'https://ivn-movies.netlify.app',
+            repoLink: 'https://github.com/ivanretamozodev/moviesV2',
+        },
+        {
             title: 'NGgames',
-            desc: 'NGgames es una PWA recoge los datos del api de rawg.io para mostrar la novedad en el mundo de los videojuegos.',
+            desc: 'NGgames es una SPA recoge los datos del api de rawg.io para mostrar la novedad en el mundo de los videojuegos.',
             img: '../../../../assets/NGgames.jpg',
             demoLink: 'https://nggames.netlify.app',
             repoLink: 'https://github.com/ivanretamozodev/gamesDB',
         },
         {
             title: 'MapWorld',
-            desc: 'Mapworld es una PWA creada en Angular que permite ver mapas,hacer zoom en el,añadir marcadores y mas,corre gracias a la api de Mapbox',
+            desc: 'Mapworld es una SPA creada en Angular que permite ver mapas,hacer zoom en el,añadir marcadores y mas,corre gracias a la api de Mapbox',
             img: '../../../../assets/MapWorld.png',
             demoLink: 'https://ngmapworld.netlify.app',
             repoLink: 'https://github.com/ivanretamozodev/Maps-App',
